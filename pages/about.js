faaa29430651ee6a8c3c4612aef6479676628ec4
Tummanoon Sripechpong <nukgroup@yahoo.com>
2021-05-25 13:42:55 -0400

    Pages and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find the lastest DJ and
+                other musicial events</p>
+                <p>version 1.0.0</p>
+
+        </div>
+    )
+}
