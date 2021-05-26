74514d2b382b1d33b77b87fd8dd74940309ed4ce
Tummanoon Sripechpong <nukgroup@yahoo.com>
2021-05-26 14:24:27 -0400

    showcase component

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
+import {useRouter} from 'next/router'
 import Header from './Header'
 import Footer from './Footer'
+import Showcase from './Showcase'
 import styles from '@/styles/Layout.module.css'
 
 
 export default function Layout
 ({title, keywords,description, children}) {
+    const router = useRouter()
     return (
         <div>
             <Head>
@@ -14,6 +17,8 @@ export default function Layout
             </Head> 
 
             <Header />
+            {router.pathname === '/' && <Showcase />}
+
             <div className={styles.container}>
             {children}
             </div>
