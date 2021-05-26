71c00f4439113379cf46f4186957b99df23809a1
Tummanoon Sripechpong <nukgroup@yahoo.com>
2021-05-26 01:12:44 -0400

    headTag and Layout

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
+import Layout from '../components/Layout'
+
 export default function AboutPage() {
     return (
-        <div>
+        <Layout title='about DJ Events'>
             <h1>About</h1>
             <p>This is an app to find the lastest DJ and
                 other musicial events</p>
                 <p>version 1.0.0</p>
-
-        </div>
+               
+        </Layout>
     )
 }
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,12 @@
+import Layout from '../../components/Layout'
+
 export default function EventPage() {
+
+   
     return (
-        <div>
+        <Layout>
             <h1>My Event</h1>
-        </div>
+          
+        </Layout>
     )
 }
