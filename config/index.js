a9940fba7145a5810a6fb9f3c9fc66b6b5b48881
Tummanoon Sripechpong <nukgroup@yahoo.com>
2021-05-29 12:40:46 -0400

    API

---
+++ config/index.js
@@ -0,0 +1,1 @@
+export const API_URL = process.env.Next_PUBLIC_API_URL || 'http://localhost:3000'
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import Layout from '@/components/Layout'
 export default function HomePage() {
   return (
    <Layout>
-     
-     <h1>Home</h1>
-     
+     <h1>Upcoming events</h1>
    </Layout>
   )
+}
+
+export async function getServerSideProps(){
+  const res = await fetch('/api/events')
 }
