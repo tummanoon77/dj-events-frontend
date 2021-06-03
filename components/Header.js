7adb1bebc698139468f47e4ce7749eddfd066b48
Tummanoon Sripechpong <nukgroup@yahoo.com>
2021-06-02 21:00:30 -0400

    Register login form

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import{FaSignInAlt,FaSignOutAlt} from 'react-icons/fa'
 import Link from 'next/link'
 import Search from './Search'
 import styles from '@/styles/Header.module.css'
@@ -22,9 +23,12 @@ export default function Header() {
                             <a>Events</a>
                         </Link>
                     </li>
+                 
                     <li>
-                        <Link href='/events/add'>
-                            <a>Add Event</a>
+                        <Link href='/account/login'>
+                            <a className='btn-secondary btn-icon'>
+                                <FaSignInAlt />Login
+                                </a>
                         </Link>
                     </li>
                 </ul>
